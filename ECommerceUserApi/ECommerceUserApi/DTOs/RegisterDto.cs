02658a1f43f20b5a3fe0d3fe627081414d38fd62
Varshajakhar2004 <varshajakhar2004@gmail.com>
2026-04-20 14:17:50 +0530

    user api

---
+++ ECommerceUserApi/ECommerceUserApi/DTOs/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceUserApi.DTOs
+{
+    public class RegisterDto
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string? Role { get; set; }
+    }
+}
---
+++ ECommerceUserApi/ECommerceWeb/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerceWeb.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
